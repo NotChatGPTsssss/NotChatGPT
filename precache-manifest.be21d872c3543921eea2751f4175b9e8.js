f51127698c287839b14464dbcab78119f6a10456
Noobs <[spongebob19119@gmail.com]>
2023-05-22 07:57:17 +0900

    Updates

---
+++ precache-manifest.be21d872c3543921eea2751f4175b9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122fb7f344cb1b85fdec51d5d406f15c",
+    "url": "/notchatgpt/index.html"
+  },
+  {
+    "revision": "9ba27484681fc43cb172",
+    "url": "/notchatgpt/static/css/main.2de0d3b8.chunk.css"
+  },
+  {
+    "revision": "16c7e4b00573bb90a2fc",
+    "url": "/notchatgpt/static/js/2.15c129b5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/notchatgpt/static/js/2.15c129b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba27484681fc43cb172",
+    "url": "/notchatgpt/static/js/main.c66e55f0.chunk.js"
+  },
+  {
+    "revision": "b53ada951ceda6708eba",
+    "url": "/notchatgpt/static/js/runtime-main.626a0b46.js"
+  }
+]);
